ba925a83267aad0750c96390d9058243e217670e
Hanah R <hanahratican@gmail.com>
2023-05-22 10:10:37 -0700

    Was able to get the current date and hour to display at the top of the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   var today = dayjs();
-  $("#currentDay").text(today.format("dddd, MMMM D, YYYY"));
+  $("#current-day").text(today.format("dddd, MMMM D, YYYY, h:mm A"));
   var currentHour = dayjs().format("H");
   // let nowMoment = moment().format('MMMM Do YYYY');
   // let displayMoment = document.getElementById('currentDay');
